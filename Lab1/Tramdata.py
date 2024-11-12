b8677608895a7630e6dfc9a9c3ccdf402ea73276
David Eriksson <davideriksson@Davids-mac.local>
2024-11-12 15:04:09 +0100

    brand new files

--- Lab1/Tramdata.py
+++ Lab1/Tramdata.py
@@ -1,1 +1,49 @@
-print("Tjooo")
+import json
+
+jsonobject = 'tramstops.json'
+lines = 'tramlines.txt'
+
+def build_tram_stops(jsonobject):
+    with open(jsonobject) as file:
+        data = json.load(file)
+        
+        for key, value in data.items():
+            tramstops = {key: {'lat': value['position'][0], 'lon': value['position'][1]}}
+            return tramstops
+            
+def build_tram_lines(lines):
+    with open(lines) as file:
+
+        print(linjer)
+        stops = []
+        for line in file:
+
+            lin = (line.split())
+            
+            stop = ' '.join(lin[:-1])
+            #print(stop)
+            
+
+        
+        #for i in stops:
+           # if i != '':
+
+        #print(stops)
+                
+        #print(stops)
+
+        #index = 1
+        #for i in range(9):
+        #    dic = {index: [line for line in file]}
+        #    index += 1
+        #    print(dic)
+#
+        #stops = []
+        #for line in file:
+        #    lin = (line.split())
+        #    
+        #    stop = (' '.join(lin[:-1]))
+            
+    #print(stops)
+
+build_tram_lines(lines)
